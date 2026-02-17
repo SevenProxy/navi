896b3b8eef9e90d95160f47e44b51c591763bfec
Proxy <flapbraga@gmail.com>
2026-02-17 00:54:27 -0300

    style(desktop): adding icons

--- src/system/desktop_environment/desktop.rs
+++ src/system/desktop_environment/desktop.rs
@@ -10,6 +10,18 @@ pub fn Desktop(props: &Props) -> Html {
 
     html! {
         <div class="w-full h-full relative">
+            <div class="text-white w-full h-full">
+                <div class="py-4 px-4 w-full max-h-full h-full">
+                    <div class="flex flex-wrap flex-col items-start">
+                    
+                        <button class="border-0 max-w-[50px] max-h-[50px] flex flex-col items-center text-center">
+                            <img class="w-full h-full" src="https://cdn.terminaltrove.com/m/8edf284d-5ce4-4d52-ab31-350866aaa79e.png" alt="foot"/>
+                            <p class="text-zinc-200">{"foot"}</p>
+                        </button>
+
+                    </div>
+                </div>
+            </div>
             { props.children.clone() }
         </div>
     }
