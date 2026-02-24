06accf3e80af583c30af97e8e6c1e39fc2f360c5
Proxy <flapbraga@gmail.com>
2026-02-23 23:45:28 -0300

    style(desktop): adding applications only

--- src/system/desktop_environment/desktop.rs
+++ src/system/desktop_environment/desktop.rs
@@ -2,6 +2,7 @@ use yew::prelude::*;
 use crate::{
     Management,
     software::{
+        TypeSoftware,
         LocalSoftware,
         get_software,
     },
@@ -22,13 +23,18 @@ pub fn Desktop(props: &Props) -> Html {
             <div class="text-white w-full h-full">
                 <div class="py-4 px-4 w-full max-h-full h-full">
                     <div class="flex flex-wrap flex-col items-start">
-                        { for get_software().iter().map(|v| html! {
-                            <button class="mb-2 border-0 max-w-[50px] max-h-[50px] flex flex-col items-center text-center relative">
-                                <button name={v.name.clone()} onclick={onclick.clone()} class="z-10 absolute top-0 left-0 w-full h-full"></button>
-                                <img class="w-full h-full" src={v.icon.clone()} alt={v.name.clone()}/>
-                                <p class="text-zinc-200">{v.name.clone()}</p>
-                            </button>
-                        })}
+                        { for get_software().iter().map(|v|
+                            match v.type_soft {
+                                TypeSoftware::Application => html! {
+                                    <button class="mb-2 border-0 max-w-[50px] max-h-[50px] flex flex-col items-center text-center relative">
+                                        <button name={v.name.clone()} onclick={onclick.clone()} class="z-10 absolute top-0 left-0 w-full h-full"></button>
+                                        <img class="w-full h-full" src={v.icon.clone()} alt={v.name.clone()}/>
+                                        <p class="text-zinc-200">{v.name.clone()}</p>
+                                    </button>
+                                },
+                                _ => html!{<></>}
+                            }
+                        )}
                     </div>
                 </div>
             </div>
